8ed8c60c3de897b788d0c6b9cdd626be231c3581
deammer <deammercraft@gmail.com>
2015-04-29 17:59:00 -0500

    Pre-Alpha #8
    Started working on a better solution for tiling platforms.

---
+++ One Button/Assets/Scripts/PlatformTiling.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformTiling : MonoBehaviour
+{
+	public Texture2D Source;
+	public int TileSize = 16;
+
+	void Start ()
+	{
+	
+	}
+	
+	void Update ()
+	{
+	
+	}
+}
