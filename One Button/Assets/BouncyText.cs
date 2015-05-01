1b9e8a4d6030442090f176e5d1b90ee2a4bc35ee
deammer <deammercraft@gmail.com>
2015-04-30 22:57:49 -0500

    Pre-Alpha #10
    Art pass on the main menu.
    Added/updated scrolling textures.

---
+++ One Button/Assets/BouncyText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncyText : MonoBehaviour
+{
+	public float Speed = 4f;
+	public float AmountX = .2f;
+	public float AmountY = .1f;
+
+	public float MaxAngle = 10f;
+	public float RotationSpeed = 5f;
+
+	private float timer;
+
+	void Start()
+	{
+		timer = Random.Range(0, 2f * Mathf.PI);
+	}
+	
+	void Update()
+	{
+		timer = Mathf.Repeat(timer + Time.deltaTime, Mathf.PI * 2f);
+	}
+
+	void OnGUI()
+	{
+		Vector3 scale = transform.localScale;
+		scale.x = 1f + Mathf.Sin(timer * Speed) * AmountX;
+		scale.y = 1f + Mathf.Cos (timer * Speed) * AmountY;
+		transform.localScale = scale;
+
+		Vector3 rotation = transform.localEulerAngles;
+		rotation.z = Mathf.Sin(timer * RotationSpeed) * MaxAngle;
+		transform.localEulerAngles = rotation;
+	}
+}
---
+++ One Button/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+
+	void Update ()
+	{
+		if (Input.anyKeyDown)
+			Application.LoadLevel("Game");
+	}
+}
