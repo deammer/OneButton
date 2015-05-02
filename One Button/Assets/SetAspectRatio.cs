68d9f1cff1ef610bcf8ebaceb67d1df234f5b9ba
deammer <deammercraft@gmail.com>
2015-05-01 23:50:35 -0500

    Pre-Alpha #12
    Messed with the screen resolution some more. Got things working somewhat
    on my Galaxy S5.

---
+++ One Button/Assets/SetAspectRatio.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetAspectRatio : MonoBehaviour
+{
+	void Awake()
+	{
+		float xFactor = Screen.width / 384f;
+		float yFactor = Screen.height / 576f;
+
+		Camera.main.rect = new Rect(0, 0, 1f, xFactor / yFactor);
+	}
+}
