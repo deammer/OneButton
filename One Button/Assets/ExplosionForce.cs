e04d07aa360ee7fde4d1afcb51469bd8f2c5834f
deammer <deammercraft@gmail.com>
2015-05-12 21:57:25 -0500

    Pre-Alpha #21
    Worked on the explosion prefab some more.
    Added a RigidBody2D.AddExplosionForce() extension.

---
+++ One Button/Assets/ExplosionForce.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionForce : MonoBehaviour
+{
+	public float radius = 5.0f;
+	public float power = 10.0f;
+	
+	void Start()
+	{
+		Vector3 explosionPos = transform.position;
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(explosionPos, radius);
+		foreach (Collider2D hit in colliders)
+		{
+			Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
+			
+			if (rb != null)
+				rb.AddExplosionForce(power, explosionPos, radius, 3f);
+		}
+	}
+}
