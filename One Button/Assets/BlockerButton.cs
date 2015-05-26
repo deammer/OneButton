2eed75844d41e1815ece2fadc71ccc4c318a7533
deammer <deammercraft@gmail.com>
2015-05-26 09:03:02 -0500

    Pre-Alpha #23
    The blocker button works. The blocker animation is in.

---
+++ One Button/Assets/BlockerButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockerButton : MonoBehaviour
+{
+	public Transform Blocker;
+
+	private bool hasBeenActivated = false;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (hasBeenActivated) return;
+
+		if (other.gameObject.tag == "Player" && PlayerController.instance.Velocity.y < -1f)
+		{
+			GetComponent<Animator>().SetBool("IsPressed", true);
+			Blocker.GetComponent<BlockerTrap>().Open();
+			hasBeenActivated = true;
+		}
+	}
+}
---
+++ One Button/Assets/BlockerTrap.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockerTrap : MonoBehaviour
+{
+	public Transform Explosion;
+
+	private Transform lockedTransform;
+	private Transform slotsTransform;
+	private Transform rightArm;
+	private Transform leftArm;
+
+	void Start()
+	{
+		lockedTransform = transform.FindChild("Locked");
+		slotsTransform = transform.FindChild("Slots");
+		rightArm = transform.FindChild("RightArm");
+		leftArm = transform.FindChild("LeftArm");
+
+		lockedTransform.gameObject.SetActive(true);
+		slotsTransform.gameObject.SetActive(false);
+		rightArm.gameObject.SetActive(false);
+		leftArm.gameObject.SetActive(false);
+	}
+
+	public void Open()
+	{
+		// disable collision
+		GetComponent<BoxCollider2D>().enabled = false;
+
+		lockedTransform.gameObject.SetActive(false);
+		slotsTransform.gameObject.SetActive(true);
+		rightArm.gameObject.SetActive(true);
+		leftArm.gameObject.SetActive(true);
+
+		if (Explosion)
+			Instantiate(Explosion, transform.position, Quaternion.identity);
+
+
+		// animate the arms
+		StartCoroutine(rightArm.MoveTo(new Vector3(5.374f, 0), .3f, Ease.ExpoIn));
+		StartCoroutine(leftArm.MoveTo(new Vector3(-5.374f, 0), .3f, Ease.ExpoIn));
+	}
+}
